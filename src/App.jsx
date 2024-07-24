f0db62e14e0b2d0b73a07bbd5e71528a7ba1c658
satyam <satyamwatts@gmail.com>
2024-07-24 23:09:46 +0530

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <>
+    <main>
       <div className="main">
         <div className="gradient"></div>
       </div>
-    </>
+      <div className="app">
+        <Home />
+        {/* <Demo /> */}
+      </div>
+    </main>
   );
 }
 
